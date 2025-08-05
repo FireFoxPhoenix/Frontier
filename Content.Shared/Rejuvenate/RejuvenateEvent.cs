e31e8faf99705129a3fce79eae90292d281556d4
FireFoxPhoenix <spacefoxphoenix@gmail.com>
2025-08-05 23:08:22 +1000

    Update RejuvenateEvent.cs

--- Content.Shared/Rejuvenate/RejuvenateEvent.cs
+++ Content.Shared/Rejuvenate/RejuvenateEvent.cs
@@ -1,5 +1,10 @@
 namespace Content.Shared.Rejuvenate;
 
-public sealed class RejuvenateEvent : EntityEventArgs
+public sealed class RejuvenateEvent(bool uncuff = true, bool resetActions = true) : EntityEventArgs // Forge edit
 {
+    // Forge-Change start
+    public bool Uncuff = uncuff;
+
+    public bool ResetActions = resetActions;
+    // Forge-Change end
 }
