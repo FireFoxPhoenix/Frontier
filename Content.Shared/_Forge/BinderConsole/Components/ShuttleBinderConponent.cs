244d7d135b2a889b58839bf304cbe3d5ba5ffaf8
FireFoxPhoenix <spacefoxphoenix@gmail.com>
2025-11-23 00:00:30 +1000

    Rename POIShuttleBinderComponent to ShuttleBinderComponent

---
+++ Content.Shared/_Forge/BinderConsole/Components/ShuttleBinderConponent.cs
@@ -0,0 +1,34 @@
+using Robust.Shared.GameStates;
+using Robust.Shared.Audio;
+using Content.Shared.Containers.ItemSlots;
+
+namespace Content.Shared._Forge.BinderConsole.Components;
+
+[RegisterComponent, NetworkedComponent, Access(typeof(SharedShuttleBinderSystem))]
+public sealed partial class ShuttleBinderComponent : Component
+{
+    public static string TargetMapSlotId = "POIShuttleBinder-targetMap";
+
+    [DataField]
+    public ItemSlot TargetMapSlot = new();
+
+    [DataField]
+    public SoundSpecifier ErrorSound =
+        new SoundPathSpecifier("/Audio/Effects/Cargo/buzz_sigh.ogg");
+
+    [DataField]
+    public SoundSpecifier ConfirmSound =
+        new SoundPathSpecifier("/Audio/Effects/Cargo/ping.ogg");
+
+    /// <summary>
+    /// The time at which the console will be able to play the deny sound.
+    /// </summary>
+    [DataField]
+    public TimeSpan NextDenySoundTime = TimeSpan.Zero;
+
+    /// <summary>
+    /// The minimum time between playing the deny sound.
+    /// </summary>
+    [DataField]
+    public TimeSpan DenySoundDelay = TimeSpan.FromSeconds(2);
+}
