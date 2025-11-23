b905c5e43c50da5446696cdffd137e5971034752
FireFoxPhoenix <spacefoxphoenix@gmail.com>
2025-11-23 11:49:21 +1000

    Update ShuttleTransmitterComponent.cs

--- Content.Shared/_Forge/ShuttleBinder/Components/ShuttleTransmitterComponent.cs
+++ Content.Shared/_Forge/ShuttleBinder/Components/ShuttleTransmitterComponent.cs
@@ -5,9 +5,6 @@ namespace Content.Shared._Forge.ShuttleBinder.Components;
 [RegisterComponent, NetworkedComponent]
 public sealed partial class ShuttleTransmitterComponent : Component
 {
-    [DataField("targetBeaconId")]
-    public string? TargetBeaconId;
-    
     [ViewVariables]
     public EntityUid? LinkedBeacon;
 }
